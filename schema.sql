0030857ecee0aac4b87f8be4b8d4ed786c3e2795
Spencer K Hagan <129553040+hagasp01@users.noreply.github.com>
2025-03-04 19:37:47 -0500

    Add files via upload
    Added schema.sql, creating the tables for database.

---
+++ schema.sql
@@ -0,0 +1,60 @@
+USE s25_lps;
+
+CREATE TABLE User (
+	uid			AUTO_INCREMENT INT PRIMARY KEY,
+	pnum			CHAR(12),
+	email			VARCHAR(50) UNIQUE NOT NULL,
+	name			VARCHAR(70),
+	created_at 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE PaymentInfo (
+	payment_username	VARCHAR(32) PRIMARY KEY,
+	payment_type		VARCHAR(16),
+	uid			INT,
+	FOREIGN KEY (uid) REFERENCES User(uid)
+);
+
+CREATE TABLE Admin (
+	admin_ID		INT PRIMARY KEY,
+	FOREIGN KEY (admin_ID) REFERENCES User(uid)
+);
+
+CREATE TABLE Ride (
+	ride_ID		AUTO_INCREMENT INT PRIMARY KEY,
+	destination		VARCHAR(100),
+	available_seats	INT,
+	dateTime		VARCHAR(18), --DDMMYYYY-00:00:00--
+	uid			INT,
+	FOREIGN KEY (uid) REFERENCES User(uid)
+);
+
+CREATE TABLE Requests (
+	PRIMARY KEY (ride_ID, passenger_ID),
+	FOREIGN KEY (ride_ID) REFERENCES Ride(ride_ID),
+	FOREIGN KEY (passenger_ID) REFERENCES User(uid)
+);
+
+CREATE TABLE Car (
+	license_plate		VARCHAR(8),
+	make			VARCHAR(16),
+	model			VARCHAR(32),
+	color			VARCHAR(16),
+	seats			INT,
+	uid			INT,
+	FOREIGN KEY (uid) REFERENCES User(uid),
+	
+);
+
+CREATE TABLE Rates (
+	reviewed_id		INT,
+	reviewer_id		INT,
+	PRIMARY KEY (reviewed_id, reviewer_id),
+	FOREIGN KEY (reviewed_id) REFERENCES User(uid),
+	FOREIGN KEY (reviewer_id) REFERENCES User(uid),
+	rating			INT,
+	review			VARCHAR(255),
+	dateTime		TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	
+);
