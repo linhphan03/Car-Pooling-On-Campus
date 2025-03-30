b6c496417db50d9ca10ecec82668eb294e0294eb
PrabeshPP <prabesh.bista369@gmail.com>
2025-03-29 22:16:41 -0400

    [added] navbar and layout for the application

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
-<?php 
-//This should be the main landing page
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gettysburg CarPool</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+        include "navbar.php"; 
+    ?>
+
+    <?php 
+        if($_GET["menu"]){
+            $menu = $_GET["menu"];
+            switch ($menu){
+                case "home":
+                    include("home.php");
+                    break;
+                case "profile":
+                    include("profile.php");
+                    break;
+                case "faq":
+                    include("faq.php");
+                    break;
+                case "about":
+                    include("about.php");
+                    break;
+            }
+        }else{
+
+        }
+    ?>
+
+    <!-- Adds JS to the Application -->
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
